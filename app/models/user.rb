9a48bfecb7eb5f7a5b2fafeaf0525b8a5a3fe344
adblanc <ablanc@student.42.fr>
2020-11-09 12:20:20 +0100

    login page + moved templates into partials

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
 	has_one_attached :avatar
 
-	validates :avatar, blob: { content_type: :image }
+	validates :avatar, blob: { content_type: :image, size_range: 1..5.megabytes }
 	validates :name, presence: true
 	validates :name, length: {minimum: 3, maximum: 32}
   	validates :login, presence: true, uniqueness: true
