a6e8665bb41f7978d94c2302ae5f26d47a2b9613
adblanc <ablanc@student.42.fr>
2020-11-12 13:00:14 +0100

    join / create channel switch

--- project/app/javascript/src/views/Chat/CreateChannelView.ts
+++ project/app/javascript/src/views/Chat/CreateChannelView.ts
@@ -1,13 +1,31 @@
 import Mustache from "mustache";
 import BaseView from "../BaseView";
 
-export default class CreateChannelView extends BaseView {
+export default class CreateJoinChannelView extends BaseView {
+  isJoinChannel: boolean;
   constructor(options?: Backbone.ViewOptions) {
     super({ className: "flex-1", ...options });
+
+    this.isJoinChannel = false;
   }
+
+  events() {
+    return {
+      "click #switch-create-join-channel": "toggle",
+    };
+  }
+
+  toggle() {
+    this.isJoinChannel = !this.isJoinChannel;
+
+    this.render();
+  }
+
   render() {
     const template = $("#createChannelTemplate").html();
-    const html = Mustache.render(template, {});
+    const html = Mustache.render(template, {
+      createChannel: !this.isJoinChannel,
+    });
     this.$el.html(html);
 
     return this;
