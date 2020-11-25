46ec6ceb41c10131008d81a8b63d40e17b1cb76a
babaoriley <helenebrulin@live.fr>
2020-11-25 18:11:13 +0100

    edit with reload

--- project/app/javascript/src/views/guild/ModifyGuildView.ts
+++ project/app/javascript/src/views/guild/ModifyGuildView.ts
@@ -47,6 +47,8 @@ export default class ModifyGuildView extends ModalView<Guild> {
     displayToast({ text: "Guild successfully updated." }, "success");
 	this.closeModal();
 	this.model.fetch();
+
+	Backbone.history.loadUrl();
   }
 
   displayError(error: string) {
