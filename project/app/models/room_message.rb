3179485a1e88fac750b49bbefd316ddd0de30137
adblanc <ablanc@student.42.fr>
2020-11-28 17:24:30 +0100

    need to redo the css but avatar url working

--- project/app/models/room_message.rb
+++ project/app/models/room_message.rb
@@ -2,7 +2,7 @@ class RoomMessage < ApplicationRecord
   belongs_to :user
   belongs_to :room, inverse_of: :room_messages
 
-  # def as_json(options)
-  #   super(options).merge(avatar_url: url_for(user.avatar))
-  # end
+  def as_json(options)
+    super(options).merge(avatar_url: Rails.application.routes.url_helpers.rails_blob_path(user.avatar, only_path: true))
+  end
 end
