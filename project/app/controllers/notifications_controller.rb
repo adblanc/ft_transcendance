a7b80144e87a4e3bd517acaec3c3afd3d750e80a
babaoriley <helenebrulin@live.fr>
2020-12-04 20:13:32 +0100

    works again

--- project/app/controllers/notifications_controller.rb
+++ project/app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
 
 	def mark_as_read
 		@notification = Notification.find(params[:id])
-		if @notification.save(read_at: Time.zone.now)
+		if @notification.update(read_at: Time.zone.now)
 			@notification
 		end
 	end
