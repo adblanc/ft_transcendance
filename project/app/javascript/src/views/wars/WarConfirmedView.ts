709c5d59fb7a2a7884583310d03e80a82fe1fdb3
babaoriley <helenebrulin@live.fr>
2020-12-19 22:51:13 +0100

    time fixes

--- project/app/javascript/src/views/wars/WarConfirmedView.ts
+++ project/app/javascript/src/views/wars/WarConfirmedView.ts
@@ -4,6 +4,7 @@ import BaseView from "../../lib/BaseView";
 import War from "src/models/War";
 import Guild from "src/models/Guild";
 import { displaySuccess } from "src/utils";
+import moment from "moment";
 
 type Options = Backbone.ViewOptions & { war: War,  opponent_id: number};
 
@@ -31,9 +32,19 @@ export default class WarConfirmedView extends BaseView {
   }
 
   render() {
+	const war = {
+		...this.war.toJSON(),
+		start: moment(this.war.get("start")).format(
+			"MMM Do YY, h:mm a"
+		),
+		end: moment(this.war.get("end")).format(
+		  "MMM Do YY, h:mm a"
+		)
+	};
+
 	const template = $("#confirmedWarTemplate").html();
 	const html = Mustache.render(template, { 
-		war: this.war.toJSON(), 
+		war: war,
 		img: this.war.get("guilds").at(this.index).get("img_url"),
 		url: `/guild/${this.opponent_id}`,
 		name: this.war.get("guilds").at(this.index).get("name")
