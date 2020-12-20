d8a806846001474651bc7132c656baedb011fb42
babaoriley <helenebrulin@live.fr>
2020-12-20 16:37:43 +0100

    another check for date

--- project/app/validators/war_date_validator.rb
+++ project/app/validators/war_date_validator.rb
@@ -3,5 +3,8 @@ class WarDateValidator < ActiveModel::Validator
 	  if record.end < record.start
 		record.errors.add :dates, ": end date cannot be before start date"
 	  end
+	  if record.end == record.start
+		record.errors.add :dates, ": start time and end time must be different"
+	  end
 	end
   end
