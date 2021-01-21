7e71605b4c3c5e3f3ce91c5461c3c0f9283569a4
babaoriley <helenebrulin@live.fr>
2021-01-21 15:50:21 +0100

    improve controller

--- project/app/controllers/friendships_controller.rb
+++ project/app/controllers/friendships_controller.rb
@@ -11,23 +11,30 @@ class FriendshipsController < ApplicationController
 	end
 
 	def accept
-		@user = User.find_by_id(params[:id])
-		@request = FriendRequest.where(requestor: @user, receiver: @current_user)
+		@other_user = User.find_by_id(params[:id])
+		return head :unauthorized if @current_user.is_friend_of?(@other_user) || @other_user.has_requested_friend(@current_user) 
+
+		@request = FriendRequest.where(requestor: @other_user, receiver: @current_user)
 		@request.destroy
-		@current_user.friends.push(@user)
-		@user.send_notification("#{@current_user.name} accepted your friend request", "/user/#{@user.id}", "friend_request")
+		@current_user.friends.push(@other_user)
+		@user.send_notification("#{@current_user.name} accepted your friend request", "/user/#{@other_user.id}", "friend_request")
 	end
 
 	def refuse
-		@user = User.find_by_id(params[:id])
-		@request = FriendRequest.where(requestor: @user, receiver: @current_user)
+		@other_user = User.find_by_id(params[:id])
+		return head :unauthorized if @current_user.is_friend_of?(@other_user) || @other_user.has_requested_friend(@current_user) 
+
+		@request = FriendRequest.where(requestor: @other_user, receiver: @current_user)
 		@request.destroy
-		@user.send_notification("#{@current_user.name} refused friend request", "/user/#{@user.id}", "friend_request")
+		@other_user.send_notification("#{@current_user.name} refused friend request", "/user/#{@other_user.id}", "friend_request")
 	end
 
 	def remove
-		@user = User.find_by_id(params[:id])
-		@current_user.friends.delete(@user)
+		@other_user = User.find_by_id(params[:id])
+		return head :unauthorized if not @current_user.is_friend_of?(@other_user) 
+
+		@other_user = User.find_by_id(params[:id])
+		@current_user.friends.delete(@other_user)
 	end
 
   end
