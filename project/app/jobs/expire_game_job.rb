8e8a2cb4726de8aa9e4b8492ca104f2a90fc2a00
babaoriley <helenebrulin@live.fr>
2021-01-28 20:45:42 +0100

    ladder swap rendered

--- project/app/jobs/expire_game_job.rb
+++ project/app/jobs/expire_game_job.rb
@@ -6,11 +6,11 @@ class ExpireGameJob < ApplicationJob
 	  if game.ladder?
 		@winner = game.initiator
 		@loser = game.opponent(@winner)
-		@winner.send_notification("Your Ladder challenge was not answered! You moved up the Ladder", "/tournaments/ladder", "game")
-		@loser.send_notification("You failed to answer a Ladder Challenge! You moved down the Ladder", "/tournaments/ladder", "game")
 		rank = @winner.ladder_rank
 		@winner.update(ladder_rank: @loser.ladder_rank)
 		@loser.update(ladder_rank: rank)
+		@winner.send_notification("Your Ladder challenge was not answered! You moved up the Ladder", "/tournaments/ladder", "game")
+		@loser.send_notification("You failed to answer a Ladder Challenge! You moved down the Ladder", "/tournaments/ladder", "game")
 	  end 
 	  game.update(status: :unanswered)
 	  ActionCable.server.broadcast("game_#{game.id}", {"event" => "expired"});
