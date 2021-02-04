b63010f295f87884f96d956046f6368a4ef95f5b
babaoriley <helenebrulin@live.fr>
2021-02-04 15:24:39 +0100

    restart registration if not full

--- project/app/jobs/tournaments/end_registration_job.rb
+++ project/app/jobs/tournaments/end_registration_job.rb
@@ -2,11 +2,22 @@ class EndRegistrationJob < ApplicationJob
 	queue_as :default
 
 	def perform(tournament)
+	  if tournament.users.count != 8
+		tournament.update(registration_end: DateTime.now + 12.hours)
+		EndRegistrationJob.set(wait_until: tournament.registration_end).perform_later(tournament)
+		User.all.each do |user|
+			user.send_notification("Registration period extension for #{tournament.name}!", "tournaments/temporary", "tournaments")
+		end
+		return
+	  end
 	  tournament.update(status: :round_one)
+	  tournament.games.each do | game |
+		game.update(status: :pending)
+	  end
 	  User.all.each do |user|
 		user.send_notification("Registrations are closed for #{tournament.name} tournament ! Play your opening matches!", "tournaments/temporary", "tournaments")
 	  end
 	  @round_length = 1
-	  RoundJob(wait_until: DateTime.now + @round_length.days).perform_later(tournament)
+	  RoundJob.set(wait_until: DateTime.now + @round_length.days).perform_later(tournament)
 	end
 end
