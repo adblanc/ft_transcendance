1bb85556e04650a9ab097f3a041fcac8d4eee5e3
Herrfalco <cadet.florian@gmail.com>
2021-02-09 16:46:53 +0400

    jobs quite persitent

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 rm -f tmp/pids/server.pid
 rails db:drop db:create db:migrate db:seed
+rails runner 'Sidekiq.redis { |conn| conn.flushdb }'
 cp ./base.rb /usr/local/bundle/gems/actionpack-6.0.3.4/lib/action_controller/base.rb
 whenever --update-crontab
 crond
