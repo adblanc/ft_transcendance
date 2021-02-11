6d4f6583282d597a2e5e4a556493f31e888bbc9a
adblanc <ablanc@student.42.fr>
2021-02-11 12:34:13 +0100

    fixed fixtures and tests

--- project/test/controllers/guilds_controller_test.rb
+++ project/test/controllers/guilds_controller_test.rb
@@ -1,24 +1,5 @@
 require 'test_helper'
 
 class GuildsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get guilds_index_url
-    assert_response :success
-  end
-
-  test "should get show" do
-    get guilds_show_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get guilds_new_url
-    assert_response :success
-  end
-
-  test "should get edit" do
-    get guilds_edit_url
-    assert_response :success
-  end
 
 end
