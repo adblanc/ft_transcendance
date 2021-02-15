b3d895e7f16bc2d24e6d13326c5bc5a0fa0f2b17
babaoriley <helenebrulin@live.fr>
2021-02-15 18:08:45 +0100

    tournaments ok up until final

--- project/app/jobs/tournaments/round_job.rb
+++ project/app/jobs/tournaments/round_job.rb
@@ -5,7 +5,7 @@ class RoundJob < ApplicationJob
 	  handle_games(tournament) 
 	  change_round(tournament)
 
-	  if !tournament.final?
+	  if not tournament.final?
 	  	RoundJob.set(wait_until: DateTime.now + 3.minutes).perform_later(tournament)
 	  end
 	end
