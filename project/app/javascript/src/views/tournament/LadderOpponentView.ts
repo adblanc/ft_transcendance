5d9824f95232ab2d1db2701e867e449d28c48357
adblanc <ablanc@student.42.fr>
2021-02-15 17:50:17 +0100

    ladder css

--- project/app/javascript/src/views/tournament/LadderOpponentView.ts
+++ project/app/javascript/src/views/tournament/LadderOpponentView.ts
@@ -7,7 +7,7 @@ import Game from "src/models/Game";
 import User from "src/models/User";
 import { eventBus } from "src/events/EventBus";
 
-type Options = Backbone.ViewOptions & { model: User, challengeable: boolean};
+type Options = Backbone.ViewOptions & { model: User; challengeable: boolean };
 
 export default class LadderOpponentView extends BaseView {
   model: User;
@@ -18,27 +18,26 @@ export default class LadderOpponentView extends BaseView {
   constructor(options?: Options) {
     super(options);
 
-	this.model = options.model;
-	this.challengeable = options.challengeable;
-	if (currentUser().get("pendingGame"))
-		this.challengeable = false;
-	if (this.model.get("id") == currentUser().get("ladder_unchallengeable")) {
-		this.challengeable = false;
-		this.justLost = true;
-	}
+    this.model = options.model;
+    this.challengeable = options.challengeable;
+    if (currentUser().get("pendingGame")) this.challengeable = false;
+    if (this.model.get("id") == currentUser().get("ladder_unchallengeable")) {
+      this.challengeable = false;
+      this.justLost = true;
+    }
 
-	this.listenTo(currentUser(), "change", this.render);
+    this.listenTo(currentUser(), "change", this.render);
   }
 
   events() {
-	return {
-	  "click #challenge-ladder": "onChallenge",
-	};
+    return {
+      "click #challenge-ladder": "onChallenge",
+    };
   }
 
   async onChallenge(e: JQuery.Event) {
     e.preventDefault();
-	this.game = new Game();
+    this.game = new Game();
     const success = await this.game.ladderChallenge(this.model.get("id"));
     if (success) {
       this.gameSaved();
@@ -47,13 +46,17 @@ export default class LadderOpponentView extends BaseView {
 
   gameSaved() {
     displaySuccess(`Waiting for ${this.model.get("login")}`);
-	currentUser().fetch();
-	this.game.createChannelConsumer();
+    currentUser().fetch();
+    this.game.createChannelConsumer();
   }
 
   render() {
     const template = $("#ladderOpponentTemplate").html();
-    const html = Mustache.render(template, {model: this.model.toJSON(), challengeable: this.challengeable, justlost: this.justLost});
+    const html = Mustache.render(template, {
+      ...this.model.toJSON(),
+      challengeable: this.challengeable,
+      justlost: this.justLost,
+    });
     this.$el.html(html);
 
     return this;
--- project/app/javascript/src/views/tournament/LadderView.ts
+++ project/app/javascript/src/views/tournament/LadderView.ts
@@ -6,43 +6,42 @@ import { currentUser } from "src/models/Profile";
 import RankedUsers from "src/collections/RankedUsers";
 import { displaySuccess } from "src/utils/toast";
 import User from "src/models/User";
-import { eventBus } from "src/events/EventBus";
-import Game from "src/models/Game";
 
 export default class LadderView extends BaseView {
-	opponents: RankedUsers;
-	max: number;
-	count: number;
+  opponents: RankedUsers;
+  max: number;
+  count: number;
 
   constructor(options?: Backbone.ViewOptions) {
-	super(options);
+    super(options);
 
-	this.opponents = new RankedUsers();
-	this.opponents.fetch();
-	this.opponents.sort();
-	this.max = 5;
-	
-	this.listenTo(this.opponents, "update", this.render);
-	this.listenTo(this.opponents, "sort", this.render);
-	this.listenTo(currentUser(), "change", this.update);
+    this.opponents = new RankedUsers();
+    this.opponents.fetch();
+    this.opponents.sort();
+    this.max = 5;
+
+    this.listenTo(this.opponents, "update", this.render);
+    this.listenTo(this.opponents, "sort", this.render);
+    this.listenTo(currentUser(), "change", this.update);
   }
 
   update() {
-	this.opponents.fetch();
-	this.opponents.sort();
-	//this.render();
+    this.opponents.fetch();
+    this.opponents.sort();
   }
 
   events() {
     return {
-	  "click #load-more": "onLoadMore",
-	  "click #accept-btn": "onAccept",
+      "click #load-more": this.onLoadMore,
+      "click #accept-btn": this.onAccept,
     };
   }
 
   async onAccept(e: JQuery.Event) {
-	e.preventDefault();
-    const success = await currentUser().get("pendingGameToAccept").acceptLadderChallenge();
+    e.preventDefault();
+    const success = await currentUser()
+      .get("pendingGameToAccept")
+      .acceptLadderChallenge();
     if (success) {
       this.gameSaved();
     }
@@ -50,26 +49,29 @@ export default class LadderView extends BaseView {
 
   gameSaved() {
     displaySuccess(`Ladder Game accepted!`);
-	currentUser().fetch();
-	currentUser().get("pendingGameToAccept").navigateToGame();
+    currentUser().fetch();
+    currentUser().get("pendingGameToAccept").navigateToGame();
   }
-
 
   renderOpponent(opponent: User) {
     var opponentView = new LadderOpponentView({
-		model: opponent,
-		challengeable: opponent.get("ladder_rank") < currentUser().get("ladder_rank")
-	});
+      model: opponent,
+      challengeable:
+        opponent.get("ladder_rank") < currentUser().get("ladder_rank"),
+    });
     this.$("#list").append(opponentView.render().el);
   }
 
   onLoadMore() {
-    var opponents = this.opponents.models.slice(this.count, this.count + this.max);
+    var opponents = this.opponents.models.slice(
+      this.count,
+      this.count + this.max
+    );
     if (opponents.length) {
-		opponents.forEach(function (item) {
-			if (item.get("id") != currentUser().get("id")) {
-				this.renderOpponent(item);
-			}
+      opponents.forEach(function (item) {
+        if (item.get("id") != currentUser().get("id")) {
+          this.renderOpponent(item);
+        }
       }, this);
       this.count += opponents.length;
     }
@@ -81,34 +83,32 @@ export default class LadderView extends BaseView {
   render() {
     const template = $("#ladderTemplate").html();
     const html = Mustache.render(template, currentUser().toJSON());
-	this.$el.html(html);
+    this.$el.html(html);
 
-	var opponents = this.opponents.first(this.max);
-    opponents.forEach(function (item) {
-		if (item.get("id") != currentUser().get("id")) {
-			this.renderOpponent(item);
-		}
-    }, this);
+    var opponents = this.opponents.first(this.max);
+    opponents.forEach((item) => {
+      this.renderOpponent(item);
+    });
     this.count = opponents.length;
     if (this.count == this.opponents.length) {
       this.$("#load-more").hide();
-	}
-	
-	if (currentUser().get("pendingGame")) {
-		if (currentUser().get("pendingGame").get("game_type") == "ladder")
-			this.$("#waiting").show();
-		else
-			this.$("#unavailable").show();
-	}
-	if (currentUser().get("pendingGameToAccept")) {
-		this.$("#accept").show();
-	}
-	if (!currentUser().get("pendingGame") && !currentUser().get("pendingGameToAccept")){
-		this.$("#default").show();
-	}
+    }
 
+    if (currentUser().get("pendingGame")) {
+      if (currentUser().get("pendingGame").get("game_type") == "ladder")
+        this.$("#waiting").show();
+      else this.$("#unavailable").show();
+    }
+    if (currentUser().get("pendingGameToAccept")) {
+      this.$("#accept").show();
+    }
+    if (
+      !currentUser().get("pendingGame") &&
+      !currentUser().get("pendingGameToAccept")
+    ) {
+      this.$("#default").show();
+    }
 
     return this;
   }
-
 }
