12a0c8d4fd731ad348c510ada6b7d0e9133f30c0
adblanc <ablanc@student.42.fr>
2021-02-16 23:32:07 +0100

    docker for production test

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -1,8 +1,9 @@
 yarn install
 rm -f tmp/pids/server.pid
-rails db:drop db:create db:migrate db:seed
+rails db:create db:migrate db:seed
 rails runner 'Sidekiq.redis { |conn| conn.flushdb }'
 cp ./base.rb /usr/local/bundle/gems/actionpack-6.0.3.4/lib/action_controller/base.rb
 whenever --update-crontab
 crond
-./bin/webpack-dev-server & bin/rails s -b 0.0.0.0
+rake webpacker:compile
+./bin/rails s -b 0.0.0.0 -e production
