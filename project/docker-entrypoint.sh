3dc3a167afef2271fcdecf4749628680b2812612
adblanc <ablanc@student.42.fr>
2021-02-17 00:16:44 +0100

    tests

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -5,5 +5,5 @@ rails runner 'Sidekiq.redis { |conn| conn.flushdb }'
 cp ./base.rb /usr/local/bundle/gems/actionpack-6.0.3.4/lib/action_controller/base.rb
 whenever --update-crontab
 crond
-rake webpacker:compile
+bundle exec rake webpacker:compile
 ./bin/rails s -b 0.0.0.0 -e production
