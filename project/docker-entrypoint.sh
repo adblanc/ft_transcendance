573a7a4049b97e6f02e99deb646b7035e246b918
adblanc <ablanc@student.42.fr>
2021-02-17 00:43:45 +0100

    assets:precompile

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -5,5 +5,5 @@ rails runner 'Sidekiq.redis { |conn| conn.flushdb }'
 cp ./base.rb /usr/local/bundle/gems/actionpack-6.0.3.4/lib/action_controller/base.rb
 whenever --update-crontab
 crond
-bundle exec rake webpacker:compile
+RAILS_ENV=production bundle exec rake assets:precompile
 ./bin/rails s -b 0.0.0.0 -e production
