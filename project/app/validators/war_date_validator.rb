a44c6e295b60dade28b85d10a9bc9331febe62f6
babaoriley <helenebrulin@live.fr>
2021-02-16 15:06:00 +0100

    if change of end or start date - war times are rechecked

--- project/app/validators/war_date_validator.rb
+++ project/app/validators/war_date_validator.rb
@@ -7,6 +7,19 @@ class WarDateValidator < ActiveModel::Validator
 		if record.end == record.start
 			record.errors.add :dates, ": start time and end time must be different"
 		end
+		if record.war_times.first.present?
+			self.check_wt(record)
+		end
+	  end
+	end
+
+	def check_wt(record)
+		@wartimes = WarTime.where(war: record)
+		for wt in @wartimes
+			if wt.start < record.start || wt.start > record.end || wt.end < record.start || wt.end > record.end
+				record.errors.add :wartime, 'must be set within the time frame of the war'
+			end
 		end
 	end
+
   end
