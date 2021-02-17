c829ca09dadb8ff69163b133c80b710ab315dd6e
adblanc <ablanc@student.42.fr>
2021-02-17 01:11:01 +0100

    layout link to css removed, backed to webpacker:compile

--- project/docker-entrypoint.sh
+++ project/docker-entrypoint.sh
@@ -5,5 +5,5 @@ rails runner 'Sidekiq.redis { |conn| conn.flushdb }'
 cp ./base.rb /usr/local/bundle/gems/actionpack-6.0.3.4/lib/action_controller/base.rb
 whenever --update-crontab
 crond
-RAILS_ENV=production bundle exec rake assets:precompile
+bundle exec rails webpacker:compile
 ./bin/rails s -b 0.0.0.0 -e production
