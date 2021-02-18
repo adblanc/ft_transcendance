f42a4225cddecbe9a57d77466ad7582918be9f70
babaoriley <helenebrulin@live.fr>
2021-02-18 16:39:42 +0100

    can't muteglobal admin info

--- srcs/app/controllers/mutes_controller.rb
+++ srcs/app/controllers/mutes_controller.rb
@@ -67,7 +67,7 @@ class MutesController < ApplicationController
 			render json: {"you" => ["must be administrator of this room"]}, status: :unprocessable_entity
 			return false;
 		elsif (@muted_user.is_room_owner?(@room))
-			render json: {"you" => ["can't mute the owner of the room"]}, status: :unprocessable_entity
+			render json: {"you" => ["can't mute the owner of the room or a global admin"]}, status: :unprocessable_entity
 			return false;
 		end
 		return true
